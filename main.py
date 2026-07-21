505b97f4b40cf41daa3a1dd5b01e1f560ce2e24f
AlonaRu27-meet <alonarubin2010@gmail.com>
2026-07-21 17:13:04 +0300

    Debuged code

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import app1
 import app2
 while True:
-    pick = input ("Which agent do you want to use? (1/2)")
-    if pick == '1':
-        app1.run_chat()
-    elif pick == '2':
-        app2.run_agent()
+    pick = input ("Which agent do you want to use? (Smart Chef/NutriGuide)")
+    if pick.lower() == 'smart chef' or pick == '1':
+        recipe_to_analyze = app1.run_chat()
+        
+        if recipe_to_analyze:
+            print("\n--- Sending recipe to NutriGuide automatically... ---")
+            nutrition_analysis = app2.run_agent(recipe_to_analyze)
+            print("\nNutriGuide Analysis:")
+            print(nutrition_analysis)
+
+    elif pick.lower() == 'nutriguide' or pick == '2':
+        app2.start_agent()
     else:
         print ("Invalid input, please try again")
